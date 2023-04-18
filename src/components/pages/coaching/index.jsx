6f8c73f7a41bbd26a32c0d7963d4f40f20347b71
buriedsignals <tom.vaillant@icloud.com>
2023-04-18 10:36:51 +0200

    coaching

--- src/components/pages/coaching/index.jsx
+++ src/components/pages/coaching/index.jsx
@@ -1,9 +1,37 @@
 import { CoachingStyle } from '@/components/pages/coaching/index.style'
+import { motion } from 'framer-motion';
 
 const Coaching = () => {
+
+  const fadeInUp = {
+  initial: {
+    y: 60,
+    opacity: 0,
+    scale: 1.1,
+    skewY: 2.5
+  },
+  animate: {
+    y: 0,
+    opacity: 1,
+    scale: 1,
+    skewY: 0,
+    transition: { duration: 0.5, ease: "easeOut" }
+  }
+};
+
   return (
     <CoachingStyle>
-      <p>testing</p>
+      <motion.div initial="initial" animate="animate" exit="exit" variants={fadeInUp}>
+        <div className="container-post-content">
+          <h1>Producer Coaching</h1>
+          <h2>Book a working session with me.</h2>
+          <p>60 minutes — $200</p>
+          <p>We&apos;ll spend the time talking about your top goals and challenges, outlining a strategy and arranging an action plan for implementation in your production or your profesionnal growth.</p>
+          <p>You&apos;ll also get a free copy of my course for creative producers once it is released in October.</p>
+          <p>The recording and any resources we create or discuss are yours to keep.</p>
+          <p>You can contact me by email <a href="mailto:tom@buriedsignals.com">here</a></p>
+        </div>
+      </motion.div>
     </CoachingStyle>
   )
 }
--- src/components/pages/coaching/index.style.jsx
+++ src/components/pages/coaching/index.style.jsx
@@ -1,5 +1,70 @@
 import styled from 'styled-components'
 
 export const CoachingStyle = styled.div`
-
+  padding-top: 163px;
+  padding-bottom: 110px;
+  .hero {
+    margin-bottom: 50px;
+  }
+  .container-post {
+    width: 100%;
+    max-width: 700px;
+    margin: 0 auto;
+    padding: 0 30px;
+  }
+  .container-post-content {
+    width: 100%;
+    max-width: 600px;
+    margin: 0 auto;
+    padding: 0 0px;
+  }
+  .back {
+    display: inline-flex;
+    align-items: center;
+    margin-bottom: 30px;
+    padding: 13.5px 0;
+    text-decoration: inherit;
+    svg {
+      margin-right: 13.5px;
+      .line {
+        transform: scale(0.5,1);
+        transition: transform 0.2s ease-in;
+      }
+    }
+    span {
+      transform: translate3D(-36px, 0, 0);
+      transition: transform 0.2s ease-in;
+    }
+    &:hover {
+      svg {
+        .line {
+          transform: scale(0.4,1);
+          transition: transform 0.25s ease-out;
+        }
+      }
+      span {
+        transform: translate3D(-44px, 0, 0);
+        transition: transform 0.25s ease-out;
+      }
+    }
+  }
+  h1, h2, h3, p, ol, ul, a {
+    margin-bottom: 13.5px;
+  }
+  ol, ul {
+    list-style: initial;
+    margin-left: 24px;
+  }
+  a {
+    text-decoration: underline;
+  }
+  iframe {
+    width: 100%;
+    height: 50vw;
+    max-height: 325px;
+  }
+  .container-post-content img {
+    margin-top: 25px;
+    margin-bottom: 25px;
+  }
 `
