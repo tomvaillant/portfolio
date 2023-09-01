2854e4b618a84c0dd2865f2f26f4ee969e418b48
buriedsignals <tom.vaillant@icloud.com>
2023-09-01 14:08:01 +0200

    small portfolio update

--- src/components/dom/descriptionWithLinks/index.jsx
+++ src/components/dom/descriptionWithLinks/index.jsx
@@ -3,9 +3,10 @@ import React from 'react';
 const DescriptionWithLinks = () => {
   return (
     <span>
-      Data storyteller and filmmaker producing visual narratives, documentaries and interactive experiences for the web. 
+      A freelance information designer and filmmaker producing visual narratives, documentaries and interactive experiences for the web.
     </span>
   );
 };
 
 export default DescriptionWithLinks;
+
