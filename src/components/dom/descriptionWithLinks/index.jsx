857a6f4ae5b6975ab295b2e00dbe23a545a52af0
buriedsignals <tom.vaillant@icloud.com>
2024-03-15 17:38:29 +0100

    escape apostrophe

--- src/components/dom/descriptionWithLinks/index.jsx
+++ src/components/dom/descriptionWithLinks/index.jsx
@@ -1,12 +1,11 @@
-import React from 'react';
+import React from 'react'
 
 const DescriptionWithLinks = () => {
   return (
     <span>
-      I'm an independent visual journalist and documentary filmmaker.
+      I`&lsquo;`m an independent visual journalist and documentary filmmaker.
     </span>
-  );
-};
+  )
+}
 
-export default DescriptionWithLinks;
-
+export default DescriptionWithLinks
